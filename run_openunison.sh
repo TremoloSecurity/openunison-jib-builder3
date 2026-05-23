296f71bd0e7d640ee75ef13796a91fb22152f63d
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2026-05-23 19:15:36 -0400

    make sure java is pid 1

--- run_openunison.sh
+++ run_openunison.sh
@@ -5,4 +5,4 @@ mkdir /tmp/quartz
 mkdir /tmp/amq
 export CLASSPATH="/usr/local/openunison/work/webapp/WEB-INF/lib/*:/usr/local/openunison/work/webapp/WEB-INF/classes:/tmp/quartz"
 echo $CLASSPATH
-java -classpath $CLASSPATH $JAVA_OPTS com.tremolosecurity.openunison.undertow.OpenUnisonOnUndertow /etc/openunison/openunison.yaml
+exec java -classpath $CLASSPATH $JAVA_OPTS com.tremolosecurity.openunison.undertow.OpenUnisonOnUndertow /etc/openunison/openunison.yaml
